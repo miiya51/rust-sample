060de180cbf37a4dd70f4d547195ce9ff687e5e3
miiya51 <fw551cp1@gmail.com>
2024-10-15 22:47:45 +0900

    feat: 5.構造体定義テスト

--- structs/src/main.rs
+++ structs/src/main.rs
@@ -1,3 +1,22 @@
+struct User {
+    username: String,
+    email: String,
+    sign_in_count: u64,
+    active: bool,
+}
+
 fn main() {
-    println!("Hello, world!");
+    
+    let mut user1 = User {
+        email: String::from("someone@example.com"),
+        username: String::from("someusername123"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    user1.email = String::from("anotheremail@example.com");
+    println!("{}", user1.email);
+    println!("{}", user1.username);
+    println!("{}", user1.active);
+    println!("{}", user1.sign_in_count);
 }

7d64c49f51ccec192500c543f820a5d2b65255d8
miiya51 <fw551cp1@gmail.com>
2024-10-24 23:36:54 +0900

    feat: 7.3 モジュールツリー

--- restaurant/src/lib.rs
+++ restaurant/src/lib.rs
@@ -1,14 +1,31 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+mod back_of_house {
+    pub struct Breakfast {
+        pub toast: String,
+        seasonal_fruit: String,
+    }
+
+    impl Breakfast {
+        pub fn summer(toast: &str) -> Breakfast {
+            Breakfast {
+                toast: String::from(toast),
+                seasonal_fruit: String::from("peaches"),
+            }
+        }
+    }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+pub fn eat_at_restaurant() {
+    // Order a breakfast in the summer with Rye toast
+    // 夏 (summer) にライ麦 (Rye) パン付き朝食を注文
+    let mut meal = back_of_house::Breakfast::summer("Rye");
+    // Change our mind about what bread we'd like
+    // やっぱり別のパンにする
+    meal.toast = String::from("Wheat");
+    println!("I'd like {} toast please", meal.toast);
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
-    }
+    // The next line won't compile if we uncomment it; we're not allowed
+    // to see or modify the seasonal fruit that comes with the meal
+    // 下の行のコメントを外すとコンパイルできない。食事についてくる
+    // 季節のフルーツを知ることも修正することも許されていないので
+    // meal.seasonal_fruit = String::from("blueberries");
 }
